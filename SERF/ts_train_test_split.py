bc51dd88f55f802fd80f30593ce6f4c35b2ef038
cbenstead <97054822+cbenstead@users.noreply.github.com>
2022-03-10 12:10:38 -0800

    unittests for ts split

--- SERF/ts_train_test_split.py
+++ SERF/ts_train_test_split.py
@@ -6,16 +6,20 @@ import datetime as dt
 
 import json_to_csv
 
+
 def uni_selection(df, variable=['ALLSKY_KT']):
     '''for univariate SARIMAX model, creates a new df with solar index'''
     X = df[variable]
+    if type(X) != pd.DataFrame:
+        raise TypeError('X is pd.Series, not DataFrame. Check parameters.')
+    else: pass
     return(X)
 
 def single_split(X, test_size=48):
     '''performs a time-series split on dataset with test size of 48 months,
     or as desired'''
     if test_size < 1 or not isinstance(test_size, int):
-        raise TypeError('test size must be positive integer.')
+        raise ValueError('test size must be positive integer.')
     elif test_size > round(len(X)*.4):
         raise ValueError('test size is large (> 40% dataset) ')
     else:
