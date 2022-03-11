cd80e43e66b243e506000c1fda5a564604b52c3b
Erick Tieu <etieu@uw.edu>
2022-03-11 11:36:22 -0800

    more tests

--- serf/hyperparam_opt.py
+++ serf/hyperparam_opt.py
@@ -13,23 +13,27 @@ from sklearn.model_selection import TimeSeriesSplit
 from sklearn.metrics import r2_score
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
+
 def loadjson(filename, column_name='ALLSKY_KT'):
     """
-    loads the geojson files and extracts the data into a dataframe  
+    loads the geojson files and extracts the data into a dataframe
+    column_name: assumes the column name is 'ALLSKY_KT' for this project.
     """
     if os.path.exists(filename):
         geodf = gp.read_file(filename)
         Idict = geodf['parameter'][0]
-        Idf =  pd.DataFrame.from_dict(Idict)
+        Idf = pd.DataFrame.from_dict(Idict)
         df = pd.DataFrame(Idf[column_name])
     else:
         raise ValueError('file does not exist')
     return df
 
+
 def rm13(df):
     """
-    remove every 13th row in a dataframe 
+    remove every 13th row in a dataframe
     to get rid of yearly average in geojson files.
+    df: dataframe
     """
     if type(df) == pd.DataFrame:
         df = df.drop(df.index[12::13])
@@ -37,11 +41,20 @@ def rm13(df):
         raise TypeError('Datatype is not a DataFrame')
     return df
 
+
 def numtodate(dfseries):
+    """
+    Adds a / to the date number because the json files format the date as
+    YEARMO instead of YEAR/MO
+    """
     dfseries = dfseries[:4] + '/' + dfseries[4:]
     return dfseries
 
+
 def Prophet_preproc(df, column_y="ALLSKY_KT"):
+    """
+    Formats the dataframe for the Prophet function.
+    """
     if column_y in df:
         df = df.rename(columns={column_y: "y"})
         df['ds'] = df.index
@@ -50,6 +63,7 @@ def Prophet_preproc(df, column_y="ALLSKY_KT"):
     else:
         raise ValueError('column name is not in dataframe')
     return df
+
 
 def TSS(df):
     """
@@ -63,14 +77,14 @@ def TSS(df):
     y_dict = {}
     kmax = 11
     tss = TimeSeriesSplit(gap=0, max_train_size=None, n_splits=kmax)
-    k=0
+    k = 0
     for train_index, test_index in tss.split(X):
         X_dev, X_test = X[train_index], X[test_index]
         y_dev, y_test = y[train_index], y[test_index]
         if k == kmax-2:
             X_train, X_val = X[train_index], X[test_index]
             y_train, y_val = y[train_index], y[test_index]
-        k=k+1
+        k = k + 1
 
     X_dict['X_dev'] = X_dev
     X_dict['X_test'] = X_test
@@ -82,19 +96,26 @@ def TSS(df):
     y_dict['y_train'] = y_train
     y_dict['y_val'] = y_val
 
-    return(X_dict,y_dict)
+    return(X_dict, y_dict)
 
-def r2score_TSS(train_data,val_data,p,d,q,P,D,Q):
-    model = SARIMAX(train_data, order=(p, d, q),seasonal_order=(P, D, Q, 12),
-                 enforce_stationarity=False, enforce_invertibility=False)
+
+def r2score_TSS(train_data, val_data, p, d, q, P, D, Q):
+    """
+    Calculates the r2 score for the validation dataset, training on the
+    training dataset.
+    pdq: the hyperparameters for the SARIMAX model.
+    """
+    model = SARIMAX(train_data, order=(p, d, q), seasonal_order=(P, D, Q, 12),
+                    enforce_stationarity=False, enforce_invertibility=False)
     model_fit = model.fit(maxiter=50, method='powell')
 
-#   start and end value is data dependent    
-    y_pred = model_fit.predict(start = 370, end = 406)
+#   start and end value is data dependent
+    y_pred = model_fit.predict(start=370, end=406)
     score = r2_score(val_data, y_pred)
     return score
 
-def objective(trial,y_train,y_val):
+
+def objective(trial, y_train, y_val):
     # define hyperparameter space
     p = trial.suggest_int("p", 0, 8)
     d = trial.suggest_int("d", 0, 1)
@@ -102,24 +123,31 @@ def objective(trial,y_train,y_val):
     P = trial.suggest_int("P", 0, 3)
     D = trial.suggest_int("D", 0, 1)
     Q = trial.suggest_int("Q", 0, 3)
-    
+
     # get the score for the hyperparameters chosen
-    score = r2score_TSS(y_train,y_val,p,d,q,P,D,Q)
-    
+    score = r2score_TSS(y_train, y_val, p, d, q, P, D, Q)
+
     return score
 
-def study_hyper(y_train,y_val,n_trials=50):
 
-    argfunction = lambda trial: objective(trial,y_train,y_val)
+def study_hyper(y_train, y_val, n_trials=50):
+    """
+    optimizes the hyperparameters for SARIMAX of a dataset using a TPE sampler.
+    """
+
+    def argfunction(trial):
+        objective(trial, y_train, y_val)
     study = optuna.create_study(
         sampler=optuna.samplers.TPESampler(), direction='maximize')
     study.optimize(argfunction, n_trials=n_trials)
-        #,y_train=y_train,y_val=y_val)
 
     results = study.trials_dataframe()
     return results
 
-def resultstocsv(results):
-    results.to_csv('../../SARIMAX_hyperparameter_results.csv') 
-    return results
 
+def resultstocsv(results):
+    """
+    pretty useless but its here, just use the function.
+    """
+    results.to_csv('../../SARIMAX_hyperparameter_results.csv')
+    return results
--- serf/tests/test_json_to_csv.py
+++ serf/tests/test_json_to_csv.py
@@ -9,8 +9,10 @@ import numpy as np
 lst = [1, 2, 3, 4, 5, 6]
 dummydf = pd.DataFrame(lst)
 
-sample2 = 'NASA/POWER_Point_Monthly_Timeseries_1981_2020_047d6155N_122d2917W_LST.csv'
-sample3 = 'NASA/POWER_Point_Monthly_Timeseries_1981_2020_047d5590N_122d7195W_LST.json'
+sample2 = ('NASA/POWER_Point_Monthly_Timeseries'
+           '_1981_2020_047d6155N_122d2917W_LST.csv')
+sample3 = ('NASA/POWER_Point_Monthly_Timeseries'
+           '_1981_2020_047d5590N_122d7195W_LST.json')
 
 data = json_to_csv.import_geojson(sample3)
 convert = json_to_csv.convert_data(data)
@@ -18,26 +20,25 @@ monthly = json_to_csv.remove_annual(convert)
 dated = json_to_csv.to_datetime(monthly)
 df = json_to_csv.geojson_to_csv(sample3)
 
+
 class TestJsonToCsv(unittest.TestCase):
-    
-    
+
     def test_import(self):
         # check that import_geojson imports a .json
-        sample2 = 'NASA/POWER_Point_Monthly_Timeseries_1981_2020_047d6155N_122d2917W_LST.csv'
+        sample2 = ('NASA/POWER_Point_Monthly_Timeseries'
+                   '_1981_2020_047d6155N_122d2917W_LST.csv')
         self.assertRaises(TypeError, json_to_csv.import_geojson, sample2)
-        
+
     def test_remove(self):
         # remove only the 13th 'month' from the pandas df. no more, no less.
         lst = [1, 2, 3, 4, 5, 6]
         dummydf = pd.DataFrame(lst)
         self.assertRaises(AttributeError, json_to_csv.remove_annual, dummydf)
-        
+
     def test_all(self):
         lst = [1, 2, 3, 4, 5, 6]
         dummydf = pd.DataFrame(lst)
-        sample3 = 'NASA/POWER_Point_Monthly_Timeseries_1981_2020_047d5590N_122d7195W_LST.json'
+        sample3 = ('NASA/POWER_Point_Monthly_Timeseries'
+                   '_1981_2020_047d5590N_122d7195W_LST.json')
         data = json_to_csv.import_geojson(sample3)
         self.assertRaises(AttributeError, json_to_csv.geojson_to_csv, dummydf)
-        
-        
-        
