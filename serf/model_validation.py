6029df269627a177d698277ea04e7bd245ea0d16
cbenstead <97054822+cbenstead@users.noreply.github.com>
2022-03-11 11:52:28 -0800

    fixed things and modified functions

--- serf/model_validation.py
+++ serf/model_validation.py
@@ -40,3 +40,29 @@ def model_val(locations, sample):
     MSE = mean_squared_error(X_test, predicted)
     r2 = r2_score(X_test, predicted)
     return MSE, r2
+
+def predict_test(locations, sample):
+     '''use pd.read_csv('Locations.csv') to access required data. 
+    Sample must be integer index of desired location.
+    inputs hyperparameters and fits model with training set, predicts last 48 months,
+    calculates MSE and r2 of predicted values against X_test (48 months)'''
+    prediction = []
+    
+    geojson = locations['filepath'][sample]
+    df = geojson_to_csv(geojson)
+    X = uni_selection(df)
+    X_train, X_test = single_split(X)
+    X_train.index = pd.DatetimeIndex(X_train.index.values,
+                                     freq=X_train.index.inferred_freq)
+    (p,d,q) = (locations['p'][sample],locations['d'][sample],locations['q'][sample])
+    (P,D,Q,s) = (locations['P'][sample],locations['D'][sample],locations['Q'][sample],12)
+    model = SARIMAX(X_train, order = (p,d,q), seasonal_order = (P,D,Q,s))
+    model_fit = model.fit(disp = False)
+    predict = model_fit.get_prediction(start='2017-01-01', end='2020-12-01')
+    
+    prediction.append(predict.predicted_mean)
+    predicted = pd.DataFrame(prediction)
+    predicted = predicted.T
+    MSE = mean_squared_error(X_test, predicted)
+    r2 = r2_score(X_test, predicted)
+    return X_test, predicted
