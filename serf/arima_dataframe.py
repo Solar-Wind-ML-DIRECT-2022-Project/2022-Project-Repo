d1614e0b5e46c84b18e92a2eda760e969380627d
ipach15 <ipach@uw.edu>
2022-03-13 15:54:39 -0700

    Plotly update

--- serf/arima_dataframe.py
+++ serf/arima_dataframe.py
@@ -1,18 +1,17 @@
 import pandas as pd
-import datetime as dt
+import datetime
 
-
-def arima_results(pred_sol):
-    df = pd.DataFrame()
-    df['Solar Ratio'] = pred_sol
+# pred_sol was a list that was the prediction from ARIMA
+def arima_results(df):
     # Associating a year and a month with the ARIMA predictions
+    df = df.rename(columns = {"solar": "Solar Ratio"})
     years = []
     year = 1984
     month = 1
     day = 1
-    # Only works for predictions from 0 to 601
-    for x in range(601):
-        if month == 12:
+    # Only works for predictions from 0 to 624
+    for x in range(624):
+        if month == 13:
             year += 1
             month = 1
         years.append(datetime.datetime(year, month, day))
