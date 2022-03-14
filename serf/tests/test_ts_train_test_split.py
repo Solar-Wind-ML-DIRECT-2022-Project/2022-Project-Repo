858cf258239e6e7ece0116f3385915b0b1e34930
Erick Tieu <etieu@uw.edu>
2022-03-14 13:38:56 -0700

    pep8

--- serf/tests/test_ts_train_test_split.py
+++ serf/tests/test_ts_train_test_split.py
@@ -8,27 +8,45 @@ import pandas as pd
 import datetime as dt
 
 
-sample = 'NASA/POWER_Point_Monthly_Timeseries_1981_2020_047d9735N_122d2038W_LST.json'
+sample = ('NASA/POWER_Point_Monthly_Timeseries_1981'
+          '_2020_047d9735N_122d2038W_LST.json')
 df = geojson_to_csv(sample)
 X = uni_selection(df)
 
 
 class TestTSSplit(unittest.TestCase):
-    
-    
+
     def test_uniselection(self):
         # from the pandas df does it create a new DataFrame
-        sample = 'NASA/POWER_Point_Monthly_Timeseries_1981_2020_047d9735N_122d2038W_LST.json'
+        sample = ('NASA/POWER_Point_Monthly_Timeseries_1981_2020'
+                  '_047d9735N_122d2038W_LST.json')
         df = geojson_to_csv(sample)
         self.assertRaises(TypeError, uni_selection, df, 'ALLSKY_KT')
-        
+
+    def test_uni_fail(self):
+        df = 'a'
+        with self.assertRaises(TypeError):
+            uni_selection(df)
+
+    def test_unit_pass(self):
+        df = geojson_to_csv(sample)
+        assert type(uni_selection(df)) == pd.DataFrame
+
     def test_split(self):
         # split dataset with reasonable numbers
-        sample = 'NASA/POWER_Point_Monthly_Timeseries_1981_2020_047d9735N_122d2038W_LST.json'
+        sample = ('NASA/POWER_Point_Monthly_Timeseries_1981_2020'
+                  '_047d9735N_122d2038W_LST.json')
         df = json_to_csv.geojson_to_csv(sample)
         X = uni_selection(df)
         self.assertRaises(ValueError, single_split, X, test_size=-2)
         self.assertRaises(ValueError, single_split, X, test_size=2.3)
         self.assertRaises(ValueError, single_split, X, test_size=350)
-        
-        
+
+    def test_split_pass(self):
+        X1, X2 = single_split(df)
+        assert type(X1) == pd.DataFrame
+        assert type(X2) == pd.DataFrame
+
+    def test_split_NaN(self):
+        with self.assertRaises(ValueError):
+            single_split('a')
